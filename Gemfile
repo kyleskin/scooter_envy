d46e78d4a535267842b404f58ed68fdf359ffe24
Kyle Skinner <kyleskin@gmail.com>
2016-06-23 09:03:05 -0600

    Added dynamic contact form

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
+gem 'mail_form', '~> 1.5', '>= 1.5.1'
 gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#homepage'
 
+  resources :contacts, only: [:new, :create]
+
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'services' => 'static_pages#services'
